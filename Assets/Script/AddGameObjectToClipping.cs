7e8e4b3e3dc0e942070a0d167666051e051cb0f9
Xumaaa <963215969@qq.com>
2023-07-16 23:48:42 +0200

    first commit

---
+++ Assets/Script/AddGameObjectToClipping.cs
@@ -0,0 +1,33 @@
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddGameObjectToClipping : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obj;
+    [SerializeField]
+    private ClippingPlane clipping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetRendererRecursive(obj);
+    }
+
+    private void GetRendererRecursive(GameObject o)
+    {
+        var renderer = o.GetComponent<MeshRenderer>();
+        if (renderer != null) 
+            clipping.AddRenderer(renderer);
+
+        if (o.transform.childCount == 0) return;
+
+        for (int i = 0; i < o.transform.childCount; i++)
+        {
+            GetRendererRecursive(o.transform.GetChild(i).gameObject);
+        }
+    }
+    
+}
---
+++ Assets/Script/AddMaterialToAsset.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMaterialToAsset : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExportMaterial(obj);
+    }
+
+    private void ExportMaterial(GameObject o)
+    {
+        Renderer renderer = o.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            Material material = renderer.material;
+            if (UnityEditor.AssetDatabase.Contains(material)) return;
+
+            Material newMaterial = new Material(material);
+            newMaterial.SetFloat("_DirectionalLight", 0);
+            newMaterial.SetFloat("_ReflectionProbe", 0);
+            newMaterial.SetFloat("_CullMode", (int)UnityEngine.Rendering.CullMode.Front);
+
+            string path = "Assets/Materials/" + material.name + ".mat";
+            UnityEditor.AssetDatabase.CreateAsset(newMaterial, path);
+        }
+        if (o.transform.childCount == 0) return;
+
+        for(int i = 0; i < o.transform.childCount; i++)
+        {
+            ExportMaterial(o.transform.GetChild(i).gameObject);
+        }
+    }
+    
+}
